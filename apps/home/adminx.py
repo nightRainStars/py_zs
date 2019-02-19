f4ea2da58ccf2fc8cde812a3a62ba38e18d3a0bd
zhangzhen <zhangzhen@ata.net.cn>
2019-02-19 09:30:41 +0800

    .

--- apps/home/adminx.py
+++ apps/home/adminx.py
@@ -3,12 +3,17 @@ from home.models import Video, CompanyInfo
 from news.models import News
 from recruit.models import Recruit
 from xadmin import views
+from xadmin.layout import Main, Fieldset, Row, Side
+from django.utils.translation import ugettext as _
+from xadmin.plugins.auth import UserAdmin
 
 
 class VideoAdmin(object):
     list_display = ['video_url', 'video_bg', 'add_time']
     search_fields = ['video_url', 'video_bg']
     list_filter = ['video_url', 'video_bg', 'add_time']
+
+    model_icon = 'fa fa-envelope'
 
 
 class CompanyInfoAdmin(object):
@@ -30,19 +35,19 @@ class BaseSetting(object):
 class GlobalSettings(object):
     site_title = "中思科技后台管理站"
     site_footer = "www.zhongsikeji.com"
+    menu_style = "accordion"  # 导航栏的收缩与展开
 
     def get_site_menu(self):
         return (
-            {'title': '我的后台', 'menus': (
-                {'title': '首页视频', 'url': self.get_model_url(Video, 'changelist')},
+            {'title': '后台', 'menus': (
+                {'title': '招贤纳士', 'url': self.get_model_url(Recruit, 'changelist')},
+                {'title': '首页视频', 'url': self.get_model_url(Video, 'changelist'), 'icon': 'fa fa-envelope'},
                 {'title': '公司信息', 'url': self.get_model_url(CompanyInfo, 'changelist')},
                 {'title': '公司动态', 'url': self.get_model_url(News, 'changelist')},
-                {'title': '招贤纳士', 'url': self.get_model_url(Recruit, 'changelist')},
-            )},
+            ), 'icon': 'fa fa-envelope'},
         )
 
 
-# 将管理器与model进行注册关联
 xadmin.site.register(Video, VideoAdmin)
 xadmin.site.register(CompanyInfo, CompanyInfoAdmin)
 
